ff20f98587a65fe5bb0f5f3f0439d628264f7bc6
tradinghasham-design <tradinghasham@gmail.com>
2026-04-01 12:42:23 +0500

    manual cors fix

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -24,3 +30,4 @@ app.use('/api/auth', authRoutes);
 app.listen(5000, () => {
   console.log('Server chal raha hai port 5000 pe');
 });
+
