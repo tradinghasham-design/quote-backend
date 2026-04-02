890b937f29fd09a55d8d12a2d557159626bf984d
tradinghasham-design <tradinghasham@gmail.com>
2026-04-02 10:53:42 +0500

    fix routes order

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
@@ -25,11 +26,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Backend Connected!' });
 });
 
-const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
 app.listen(5000, () => {
   console.log('Server chal raha hai port 5000 pe');
 });
-
-
